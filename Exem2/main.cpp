d52c137f01116cc8bf87de07379a6721a0562b9d
NIKX <tiger.zubairov@gmail.com>
2024-05-15 00:19:12 +0300

    я ем хлеб

--- Exem2/main.cpp
+++ Exem2/main.cpp
@@ -4,107 +4,105 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
-using namespace std;
-bool isBetween(double value, double a, double b, double epsilon) {
-    // Проверяем, что value находится в интервале от a до b, с учетом эпсилона
-    return (value + epsilon >= a && value - epsilon <= b) || (value - epsilon <= a && value + epsilon >= b);
-}
 
+using namespace std;
+
+void output_interval_series(const string&, vector<double>&);
+void output_writing_in_file(const string&);
 void read_file(const string& ,vector<double>&);
+
 int main() {
+
     try{
         vector<double> arr;
-        read_file("C:/Users/tiger/CLionProjects/untitled2/file_stream.txt", arr);
-        sort(arr.begin(), arr.end());
-        double front_el = arr.front();
-        double back_el = arr.back();
-        double R =  back_el - front_el; // different between min element and max element
-        int n = arr.size(); // count interval
-        int k = 1+3.322*log10(n); // optimal count interval
 
-        double sum_n = 0;
-        double sum_relative_density = 0;
-
-        double h = R/k;
-        double sum = front_el;
-        double epsilon =1e-12;
-
-        do{
-
-            double count = 0;
-            sum += h;
-            cout << "[" << sum-h << " " << sum << "] ";
-            double x = sum - h/2;
-            cout << x << " ";
-            for(double & it : arr) {
-                if( ( (it  > sum-h ) && (it  < sum ) ) || (( abs(it - (sum-h) ) <= epsilon ) || ( abs(it - sum ) <= epsilon )) ){ count++; }
-
-            }
-            cout << count << " ";
-            double frequency_density = ((double) count) / h;
-            double relative_density = count / n ;
-            double relative_frequency_density = relative_density / h;
-
-            cout << frequency_density <<  " " <<  relative_density<<  " " << relative_frequency_density <<  endl;
-            sum_n += count;
-            sum_relative_density += relative_density;
-        } while (  sum  < back_el );
-        cout << sum_n << " ";
-        cout << sum_relative_density << " ";
-        system("python C:/Users/tiger/CLionProjects/untitled2/main.py");
+        read_file("./file_stream.txt", arr);
+        sort(arr.begin(), arr.end()); 
+        output_interval_series("./data.txt", arr);
+        system("python main.py");
+        return 0;
     }
-
-    catch(invalid_argument& e){
+    catch(const runtime_error& e){
         cerr << e.what() << endl;
         return -1;
     }
 
-    return 0;
 }
 
-void read_file(const string& file_name, vector<double>& vector_in){
+void read_file(const string& filepath, vector<double>& vector_in){
     string line;
-    ifstream file(file_name);
-    if ( file.is_open() )
+    ifstream inf(filepath);
+    if (!inf.is_open()){
+        throw runtime_error("Error file: Not found file or not allocate of memory");
+    }
+
+    while (getline(inf, line))
     {
-
-        while (getline(file, line))
-        {
-
-            size_t pos;
-            do{
-                pos = line.find(',');
-                vector_in.push_back(stod(line.substr(0,pos)));
-                line.erase(0, pos+1);
-            }while(pos != string::npos);
-
-        }
+        size_t pos;
+        do{
+            pos = line.find(',');
+            vector_in.push_back(stod(line.substr(0,pos)));
+            line.erase(0, pos+1);
+        }while(pos != string::npos);
     }
-    else{
-        throw invalid_argument("Error file: Not found file or not allocate of memory");
-    }
-    file.close();
+    inf.close();
 }
-void output_writing_in_file(const string& file_name, vector<double>& vector_in){
+
+void output_interval_series(const string& filepath, vector<double>& arr){
     string line;
-    ifstream file(file_name);
-    if ( file.is_open() )
-    {
+    ofstream out(filepath);
+    
+    if (!out.is_open()){
+        throw runtime_error("Error file: Not found file or not allocate of memory");
+    }
+    out << "intervals xi ni ni/h wi wi/h" << endl;
 
-        while (getline(file, line))
-        {
+    double front_el = arr.front(); // самый наименьший член коллекции
+    double back_el = arr.back(); // самый наибольший член коллекции
 
-            size_t pos;
-            do{
-                pos = line.find(',');
-                vector_in.push_back(stod(line.substr(0,pos)));
-                line.erase(0, pos+1);
-            }while(pos != string::npos);
+    double R =  back_el - front_el; // размах вариации (длина общего интервала)
+    int n = arr.size(); // количество интервалов
+    int k = 1+3.322*log10(n); // формула Стерджеса ( оптимальное разбиение интервалов )
 
+    double sum_n = 0;
+    double sum_relative_density = 0;
+
+    double h = R/k; // длина одного интервала
+
+    double sum = front_el;
+    double epsilon =1e-12;
+
+    do{
+
+        double count = 0;
+        sum += h;
+
+        // интервалы
+        string interval = "[ " + to_string(sum-h) + ' ' +  to_string(sum) + " ]";
+
+        // расчитывание середин (xi)
+        string x = to_string(sum - h/2);
+
+        // количество чисел принадлежащих данному интервалу
+        for(double & it : arr) {
+            if( ( (it  > sum-h ) && (it  < sum ) ) || (( abs(it - (sum-h) ) <= epsilon ) || ( abs(it - sum ) <= epsilon )) ){ count++; }
         }
-    }
-    else{
-        throw invalid_argument("Error file: Not found file or not allocate of memory");
-    }
-    file.close();
+
+        // плотность частот 
+        string frequency_density =  to_string(count / h);
+        
+        //　относительные частоты
+        string relative_density =  to_string( ((double) count / n ));
+        
+        // плотность относительных частот 
+        string relative_frequency_density =  to_string(  ( (double) count / n) / h );
+    
+        out << interval  + ' ' +   x + ' ' + to_string(count) + ' ' + frequency_density + ' ' +  relative_density  + ' ' +   relative_frequency_density << endl;
+        sum_n += count;
+        sum_relative_density += (count / n);
+
+    } while (  sum  < back_el );
+
+    //out <<  to_string( sum_n) + ' ' + to_string(round(sum_relative_density)) << endl;
+    out.close(); 
 }
--- Exem2/main.py
+++ Exem2/main.py
@@ -1,7 +1,7 @@
-'''import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import pylab
-print(plt.get_backend())
+
 f = open('./data.txt', 'r')
 f.readline();
 xi = [];
@@ -12,22 +12,21 @@ ni_division_h = [];
 for line_file in f:
     lst_tmp = line_file.split(" ")
     lst_tmp.pop(0)
-    lst_tmp.pop(0)
+    lst_tmp.pop(2)
     lst_tmp[-1] = lst_tmp[-1][:-1]
-    xi.append(float(lst_tmp[0]))
-    ni.append(float(lst_tmp[1]))
-    ni_division_h.append(float(lst_tmp[2]))
-    wi.append(float(lst_tmp[3]))
-    wi_division_h.append(float(lst_tmp[4]))
+    h = float(lst_tmp[1]) - float(lst_tmp[0])
+    xi.append(float(lst_tmp[2]))
+    ni.append(float(lst_tmp[3]))
+    ni_division_h.append(float(lst_tmp[3]))
+    wi.append(float(lst_tmp[4]))
+    wi_division_h.append(float(lst_tmp[5]))
 
-print(ni)
-print(wi)
 pylab.subplot(221)
-pylab.bar(xi,ni_division_h, width=0.96,edgecolor='black', color="violet" )
+pylab.bar(xi,ni_division_h, width=h,edgecolor='black', color="violet" )
 pylab.title ("Гистограмма частот")
 
 pylab.subplot(222)
-pylab.bar(xi, wi_division_h, width=0.96, edgecolor='black', color="violet")
+pylab.bar(xi, wi_division_h, width=h, edgecolor='black', color="violet")
 pylab.title ("Гистограмма относительных частот")
 
 pylab.subplot(223)
@@ -38,13 +37,12 @@ pylab.subplot(224)
 pylab.plot(xi, wi, color='green')
 pylab.title ("Полигон относительных частот")
 
-pylab.show()'''
-import os
+pylab.show()
+
+'''import os
 import eel 
 try:
     eel.init(f'{os.path.dirname(os.path.realpath(__file__))}/web')
     eel.start("index.html",size=(1250, 850))
 except Exception as e:
-    print(e)
-finally:
-    print("\nКонец программы.")
+    print(e)'''
